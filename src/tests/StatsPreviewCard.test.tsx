af6de58093a3197f0374e8479201371a83536c0c
hans <hans.krebs@me.com>
2021-08-31 17:29:45 +0200

    Fix test.

--- src/tests/StatsPreviewCard.test.tsx
+++ src/tests/StatsPreviewCard.test.tsx
@@ -1,9 +1,9 @@
 import {render, screen} from '@testing-library/react';
-import RandomContent from '../components/ParallaxContent';
+import MarginsLeftRight from '../components/MarginsLeftRight';
 
 describe('<StatsPreviewCard />', () => {
   it('should render header text', () => {
-    render(<RandomContent />);
+    render(<MarginsLeftRight ><h1>foo</h1></MarginsLeftRight>);
     expect(screen.getByText(/Get/i)).toBeInTheDocument();
     expect(screen.getByText(/insights/i)).toBeInTheDocument();
     expect(screen.getByText(/that help your business grow./i)).toBeInTheDocument();
