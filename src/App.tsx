22a7f72eb579544e40d8922e90801545dae16675
hans <hans.krebs@me.com>
2021-08-31 17:19:22 +0200

    Clean implementation of landing page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import CustomTheme from './styles/Theme';
 import {ThemeProvider} from '@material-ui/core';
 import FullHeightWrapper from './components/FullHeightWrapper';
-import ParallaxContent from './components/ParallaxContent';
-import {ParallaxProvider} from 'react-scroll-parallax';
 import LandingPage from './components/LandingPage';
 import hansImg from './assets/IMG_9921_matte_look_1440.jpeg'
 import useBreakPoint from './hooks/useBreakPoint';
@@ -11,36 +9,9 @@ function App() {
   const downPhone = !useBreakPoint('up', 'phone', undefined, CustomTheme);
   return (
     <ThemeProvider theme={CustomTheme}>
-      <ParallaxProvider>
-        <FullHeightWrapper backgroundColor="#48484a" backgroundImg={hansImg} height={downPhone ? "100vh" : "100vh"} ><LandingPage /></FullHeightWrapper>
-        <FullHeightWrapper backgroundColor="white">
-          <ParallaxContent yOffset={
-            downPhone
-              ? (window.visualViewport.width > 765
-                ? [-30 - (window.visualViewport.width - 375) / 8, 30 - (window.visualViewport.width - 375) / 8]
-                : [0, 0]
-              )
-              : [-50 - (window.visualViewport.width - 1280) / 10, 60 + (window.visualViewport.width - 1280) / 10]
-          }>
-            <h1>
-              I am a <span>software architect</span> passionate about <i>web development</i> and <i><label>frontend desgin</label></i>.
-            </h1>
-            <h2 style={{color: "#48484a"}}>
-              Besides programming, I enjoy 📸, 🚴‍♂️, good 🍷 and outdoor 🌿.
-            </h2>
-          </ParallaxContent>
-        </FullHeightWrapper>
-        <FullHeightWrapper backgroundColor="#48484a">
-          <ParallaxContent yOffset={[-50, 50]}>
-            <h1>Works 📸</h1>
-            <h2 >
-              As my preferred techstack I'm using <span>React</span> with <span>Typescript</span> and <span>Material-UI</span> 💻.
-            </h2>
-          </ParallaxContent>
-        </FullHeightWrapper>
-        <FullHeightWrapper backgroundColor="black"><ParallaxContent yOffset={[-200, 200]}><h1>Experience 🔬</h1></ParallaxContent></FullHeightWrapper>
-        <FullHeightWrapper backgroundColor="white"><ParallaxContent yOffset={[-200, 200]}><h1>Contact & Download CV 🎙</h1></ParallaxContent></FullHeightWrapper>
-      </ParallaxProvider>
+      <FullHeightWrapper backgroundColor="#48484a" backgroundImg={hansImg} height={downPhone ? "100vh" : "100vh"} >
+        <LandingPage />
+      </FullHeightWrapper>
     </ThemeProvider>
   );
 }
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,16 +1,9 @@
-import {Parallax} from 'react-scroll-parallax'
 import Grid from '@material-ui/core/Grid'
 import {makeStyles} from '@material-ui/core/styles';
 import useBreakPoint from '../hooks/useBreakPoint';
 import hansImgMobile from '../assets/IMG_9921_matte_look_avatar.jpeg'
 
 const SPLIT = 65;
-const OFFSETS = {
-    desktop: [-80, 40],
-    mobile: window.visualViewport.width < 450
-        ? [-25 - (window.visualViewport.width - 375) / 10, 20 - (window.visualViewport.width - 375) / 10]
-        : [10, 0]
-}
 
 const useStyles = makeStyles({
     fullWidth: {
@@ -36,28 +29,24 @@ export default function LandingPage() {
                         backgroundColor: 'white', height: '30vh', width: '100%',
                         position: 'absolute', top: 0
                     }} ></div>
-                    <Parallax className={classes.fullWidth} y={OFFSETS.mobile}>
-                        <Grid container direction="column" justifyContent="center" alignItems="center">
-                            <div style={{backgroundImage: `url(${hansImgMobile})`, boxShadow: '0 0 15px 15px #48484a inset', backgroundSize: 'cover', borderRadius: '50%'}}>
-                                <img style={{filter: 'opacity(0%)', width: '80vw'}} src={hansImgMobile} alt="Hans" />
-                            </div>
-                            <h1 style={{color: 'white'}} >I'm Hans </h1>
-                            <h2 style={{paddingBottom: '60px', marginLeft: '30px', marginRight: '30px', textAlign: 'center'}}>Welcome To My About Me Page!</h2>
-                        </Grid>
-                    </Parallax >
-                </>
-                : <Parallax className={classes.fullWidth} y={OFFSETS.desktop}>
-                    <Grid container >
-                        <Grid item className={classes.left}>
-                            {/* empty */}
-                        </Grid>
-                        <Grid item className={classes.right} container justifyContent="center" direction="column" >
-                            <h3 >Hello 👋</h3>
-                            <h1 >I'm Hans </h1>
-                            <h2>Welcome To My About Me Page!</h2>
-                        </Grid>
+                    <Grid container direction="column" justifyContent="center" alignItems="center">
+                        <div style={{backgroundImage: `url(${hansImgMobile})`, boxShadow: '0 0 15px 15px #48484a inset', backgroundSize: 'cover', borderRadius: '50%'}}>
+                            <img style={{filter: 'opacity(0%)', width: '80vw'}} src={hansImgMobile} alt="Hans" />
+                        </div>
+                        <h1 style={{color: 'white'}} >I'm Hans </h1>
+                        <h2 style={{paddingBottom: '60px', marginLeft: '30px', marginRight: '30px', textAlign: 'center'}}>Welcome To My About Me Page!</h2>
                     </Grid>
-                </Parallax >
+                </>
+                : <Grid container >
+                    <Grid item className={classes.left}>
+                        {/* empty space filler */}
+                    </Grid>
+                    <Grid item className={classes.right} container justifyContent="center" direction="column" >
+                        <h3 >Hello 👋</h3>
+                        <h1 >I'm Hans </h1>
+                        <h2>Welcome To My About Me Page!</h2>
+                    </Grid>
+                </Grid>
             }
         </>
     )
